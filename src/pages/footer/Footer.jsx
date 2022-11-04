30031febf84f0fc6c9bc9ac87947bfd87b86d236
DESKTOP-870CDBC\SUNHU <kku98057@naver.com>
2022-11-04 13:53:29 +0900

    모바일작업

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,32 +1,31 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
-  
   const goToTwitter = () => {
-    document.location.href = 'https://twitter.com/Global_LoD';
-  }
+    document.location.href = "https://twitter.com/Global_LoD";
+  };
 
   const goToDiscord = () => {
-    document.location.href = 'https://discord.gg/lordofdragons';
-  }
+    document.location.href = "https://discord.gg/lordofdragons";
+  };
 
   const goToMedium = () => {
-    document.location.href = 'https://medium.com/@Global_LoD';
-  }
+    document.location.href = "https://medium.com/@Global_LoD";
+  };
 
   return (
-    <div className="footer">
-        <a className="footer_logo"></a>
-        <div className="sns_section">
-            <div className='company_info'>SOTEM MOBILE Co., Ltd.</div>
-            <div className='sns_link_section'>
-                <a className='twitter' onClick={goToTwitter}></a>
-                <a className='discord' onClick={goToDiscord}></a>
-                <a className='medium' onClick={goToMedium}></a>
-            </div>
+    <footer className="footer">
+      <a className="footer_logo"></a>
+      <div className="sns_section">
+        <div className="company_info">SOTEM MOBILE Co., Ltd.</div>
+        <div className="sns_link_section">
+          <a className="twitter" onClick={goToTwitter}></a>
+          <a className="discord" onClick={goToDiscord}></a>
+          <a className="medium" onClick={goToMedium}></a>
         </div>
-        <a className="btn_top"></a>
-    </div>
+      </div>
+      <a className="btn_top"></a>
+    </footer>
   );
 }
 
--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,47 +1,150 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Header.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
 
 function Header(props) {
   const [menu, setMenu] = useState("");
-
+  const [mobMenu, setMobMenu] = useState(true);
   useEffect(() => {
-      if(menu === "home") {
-        console.log("goToHome");
-      } else if (menu === "game") {
-        console.log("goToGame");
-      } else if (menu === "inventory") {
-        console.log("goToInventory");
-      }
+    if (menu === "home") {
+      console.log("goToHome");
+    } else if (menu === "game") {
+      console.log("goToGame");
+    } else if (menu === "inventory") {
+      console.log("goToInventory");
+    }
   }, []);
 
   const navigator = useNavigate();
 
   const goToHome = () => {
-        console.log("goToGame");
-    navigator('/');
-  }
+    console.log("goToGame");
+    navigator("/");
+  };
 
-  const goToGame = event => {
+  const goToGame = (event) => {
     console.log("goToGame");
     navigator("/game");
-  }
+  };
 
-  const goToInventory = event => {
+  const goToInventory = (event) => {
     console.log("goToInventory");
-    navigator('/inventory');
-  }
+    navigator("/inventory");
+  };
   // console.log(`현재 위치 ${props.cursor}`);
   return (
     <div className="menu">
-        <a className="logo"></a>
-        {props.cursor === "marketplace" ? <div className='cursor_section'> <div className="c_marketplace" onClick={goToHome}>MARKETPLACE</div> <p className='cursor'/> </div> : <div className="marketplace" onClick={goToHome}>MARKETPLACE</div> }
-        <a className="comming">COMMING SOON</a> 
-        <a className="comming">COMMING SOON</a>
-        {props.cursor === "game" ? <div className='cursor_section'><div className="c_game" onClick={goToGame}>GAME</div> <p className='cursor'/> </div> : <div className="game" onClick={goToGame}>GAME</div> }
-        {props.cursor === "document" ? <div className='cursor_section'><div className="c_document">DOCUMENT</div> <p className='cursor'/> </div>: <div className="document">DOCUMENT</div>}
-        {props.cursor === "inventory" ? <div className='cursor_section'><div className='c_inventory' onClick={goToInventory}>INVENTORY</div> <p className='cursor'/> </div> : <div className="inventory" onClick={goToInventory}>INVENTORY</div>}
-        <a className="wallet"></a>
+      <a className="logo"></a>
+      {props.cursor === "marketplace " ? (
+        <div className="cursor_section menu_list">
+          {" "}
+          <div className="c_marketplace menu_list" onClick={goToHome}>
+            MARKETPLACE
+          </div>{" "}
+          <p className="cursor" />{" "}
+        </div>
+      ) : (
+        <div className="marketplace menu_list" onClick={goToHome}>
+          MARKETPLACE
+        </div>
+      )}
+      <a className="comming menu_list">COMMING SOON</a>
+      <a className="comming menu_list">COMMING SOON</a>
+      {props.cursor === "game" ? (
+        <div className="cursor_section menu_list">
+          <div className="c_game" onClick={goToGame}>
+            GAME
+          </div>{" "}
+          <p className="cursor" />{" "}
+        </div>
+      ) : (
+        <div className="game menu_list" onClick={goToGame}>
+          GAME
+        </div>
+      )}
+      {props.cursor === "document" ? (
+        <div className="cursor_section menu_list">
+          <div className="c_document">DOCUMENT</div> <p className="cursor" />{" "}
+        </div>
+      ) : (
+        <div className="document menu_list">DOCUMENT</div>
+      )}
+      {props.cursor === "inventory" ? (
+        <div className="cursor_section menu_list">
+          <div className="c_inventory" onClick={goToInventory}>
+            INVENTORY
+          </div>{" "}
+          <p className="cursor" />{" "}
+        </div>
+      ) : (
+        <div className="inventory menu_list" onClick={goToInventory}>
+          INVENTORY
+        </div>
+      )}
+      <a className="wallet menu_list"></a>
+      <div className="mobMenu_btn">
+        <div className="mobMenu_btn_list"></div>
+        <div className="mobMenu_btn_list"></div>
+        <div className="mobMenu_btn_list"></div>
+      </div>
+
+      {/* 모바일메뉴 */}
+      <div className="mobMenu">
+        <div className="mobMenu_wrap">
+          {props.cursor === "marketplace" ? (
+            <div className="cursor_section mobMenu_list">
+              {" "}
+              <div className="c_marketplace" onClick={goToHome}>
+                MARKETPLACE
+              </div>{" "}
+              <p className="cursor" />{" "}
+            </div>
+          ) : (
+            <div className="marketplace" onClick={goToHome}>
+              MARKETPLACE
+            </div>
+          )}
+          <a className="comming mobMenu_list">
+            COMMING <br className="mob_br" /> SOON
+          </a>
+          <a className="comming mobMenu_list">
+            COMMING <br className="mob_br" /> SOON
+          </a>
+          {props.cursor === "game" ? (
+            <div className="cursor_section mobMenu_list">
+              <div className="c_game" onClick={goToGame}>
+                GAME
+              </div>{" "}
+              <p className="cursor" />{" "}
+            </div>
+          ) : (
+            <div className="game mobMenu_list" onClick={goToGame}>
+              GAME
+            </div>
+          )}
+          {props.cursor === "document" ? (
+            <div className="cursor_section mobMenu_list">
+              <div className="c_document">DOCUMENT</div>{" "}
+              <p className="cursor" />{" "}
+            </div>
+          ) : (
+            <div className="document mobMenu_list">DOCUMENT</div>
+          )}
+          {props.cursor === "inventory" ? (
+            <div className="cursor_section mobMenu_list">
+              <div className="c_inventory" onClick={goToInventory}>
+                INVENTORY
+              </div>{" "}
+              <p className="cursor" />{" "}
+            </div>
+          ) : (
+            <div className="inventory mobMenu_list" onClick={goToInventory}>
+              INVENTORY
+            </div>
+          )}
+          <a className="wallet mobMenu_list"></a>
+        </div>
+      </div>
     </div>
   );
 }
